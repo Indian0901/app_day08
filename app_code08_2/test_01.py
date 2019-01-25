bf39d3e1ec28c3b2a8fd1c117f8fea7b5010b098
Indian0901 <46989343+Indian0901@users.noreply.github.com>
2019-01-25 19:52:42 +0800

    Update test_01.py

--- app_code08_2/test_01.py
+++ app_code08_2/test_01.py
@@ -1,7 +1,10 @@
+import allure
 class Test_001:
+    @allure.step(title="步骤1")
     def test_001_1(self):
         print('--->test_001_1')
         assert True
+    @allure.step(title="步骤2")
     def test_001_2(self):
         print('--->test_001_2')
         assert False
